ac4b826b5e5900c391bb9a41022baafc6a1ba8d5
accors <61780738+accors@users.noreply.github.com>
2021-11-20 17:52:47 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ do
 ping -c 3 -W 5 youtube.com &> /dev/null
 if [ "$?" != "0" ]; then
 echo "不可访问外网，设置GitHub镜像"
-git config --global url."https://hub.fastgit.org/".insteadOf "https://github.com/"
+git config --global url."https://github.com.cnpmjs.org/".insteadOf "https://github.com/"
 git config protocol.https.allow always
 break
 else
